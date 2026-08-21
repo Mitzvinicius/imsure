72495adee4f82151bc10af2caaa1f73b35c519ed
Carlos <carlos.dallanora@neomind.com.br>
2026-08-21 20:09:01 -0300

    server supabase

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -4,7 +4,32 @@ import { cookies } from "next/headers";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
-export const createClient = (cookieStore: Awaited<ReturnType<typeof cookies>>) => {
+export async function createClient() {
+  const cookieStore = await cookies();
+
+  return createServerClient(
+      supabaseUrl!,
+      supabaseKey!,
+      {
+        cookies: {
+          getAll() {
+            return cookieStore.getAll();
+          },
+          setAll(cookiesToSet, _headers) {
+            try {
+              cookiesToSet.forEach(({ name, value, options}) =>
+              cookieStore.set(name, value, options)
+              )
+            } catch {
+
+            }
+          },
+        },
+      }
+  )
+}
+
+/* export const createClient2 = (cookieStore: Awaited<ReturnType<typeof cookies>>) => {
   return createServerClient(
     supabaseUrl!,
     supabaseKey!,
@@ -25,4 +50,4 @@ export const createClient = (cookieStore: Awaited<ReturnType<typeof cookies>>) =
       },
     },
   );
-};
+};*/
